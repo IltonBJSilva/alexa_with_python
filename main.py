2d9b680d88d739929cf6fc66dbf1e23309f188db
IltonBJSilva <xlsonplay@gmail.com>
2023-12-08 15:58:56 -0300

    Primeira versão do comando por voz, reconhecimento e retorno de fala

--- main.py
+++ main.py
@@ -9,11 +9,26 @@ audio = sr.Recognizer()
 maquina = pyttsx3.init()
 
 
-print(sr.Microphone().list_microphone_names())
+#print(sr.Microphone().list_microphone_names())
+
+def executa_comando():
+    try:
+        with sr.Microphone(2) as source:
+            print('Ouvindo audio...') # pra saber se esta ouvindo a gente
+            voz = audio.listen(source) # escutar o audio
+            comando = audio.recognize_google(voz, language='pt-BR') # receber comando
+            comando = comando.lower() # deixar tudo em minusculo
+            if 'tina' in comando:
+                comando = comando.replace('tina', '') # tirar o nome tina
+                maquina.say(comando) # Fala
+                maquina.runAndWait() # da run no programa
+
+    except:
+        print('Microfone não funciona corretamente')
 
 
+    return comando
 
-comando_voz_usuario()
 
 
 
